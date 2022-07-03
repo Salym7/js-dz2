f40b0bc6b3c5c66c0d75caf5e0a5480c2c0ff207
Salym7 <salym3k@gmail.com>
2022-07-03 21:36:23 +0300

    up

---
+++ js/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Task #1
+
+let name;
+let admin;
+name = 'Джон';
+admin = name;
+alert(admin);
+
+// Task #2
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'Max';
+
+// Task #3
+
+let sum = '10' + 10;
+console.log(sum)
+console.log(typeof sum)
+
+
+
+
+
+
+
+
+
+
+
+
